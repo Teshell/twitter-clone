91764968ffd607d173bd013427343baceff4e202
Teshell <fayrongeek@gmail.com>
2022-04-07 14:33:29 +0100

    input component design

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,5 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import Input from "./Input";
 
 const Feed = () => {
   return (
@@ -10,6 +11,8 @@ const Feed = () => {
           <SparklesIcon className="h-5 text-white" />
         </div>
       </div>
+
+      <Input />
     </div>
   );
 };

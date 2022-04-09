826f4f4b9254801f5df36fa8736c5febe21f81cf
Teshell <fayrongeek@gmail.com>
2022-04-09 15:26:58 +0100

    next auth login

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from "next/head";
+
 import Feed from "../components/Feed";
+import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
 
-export default function Home() {
+import { getProviders, getSession, useSession } from "next-auth/react";
+
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div>
       <Head>
@@ -19,4 +27,25 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
